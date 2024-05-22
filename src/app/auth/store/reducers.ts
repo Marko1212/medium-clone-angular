173d9130a22b23374da8d42209873eaf65592239
Marko Askovic <marko.askovic@astree-software.fr>
2024-05-22 13:31:25 +0200

    get current user

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -44,6 +44,20 @@ const authFeature = createFeature({
       isSubmitting: false,
       validationErrors: action.errors,
     })),
+    on(authActions.getCurrentUser, (state, action) => ({
+      ...state,
+      isLoading: true,
+    })),
+    on(authActions.getCurrentUserSuccess, (state, action) => ({
+      ...state,
+      isLoading: false,
+      currentUser: action.currentUser,
+    })),
+    on(authActions.getCurrentUserFailure, (state) => ({
+      ...state,
+      isLoading: false,
+      currentUser: null,
+    })),
     on(routerNavigationAction, (state) => ({
       ...state,
       validationErrors: null,
