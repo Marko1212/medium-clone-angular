99444aada7face9b52ee24687a991d50b0de163f
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-05 13:40:42 +0200

    continued, todo: finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,4 +16,9 @@ export const appRoutes: Route[] = [
     loadChildren: () =>
       import('src/app/globalFeed/globalFeed.routes').then((m) => m.routes),
   },
+  {
+    path: 'feed',
+    loadChildren: () =>
+      import('src/app/yourFeed/yourFeed.routes').then((m) => m.routes),
+  },
 ];
