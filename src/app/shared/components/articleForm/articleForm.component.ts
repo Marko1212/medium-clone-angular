a4cba109c45c7c997055259b0653b6a9321015bd
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-11 13:54:12 +0200

    article form started

---
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-create-article',
+  templateUrl: './articleForm.component.html',
+  standalone: true,
+})
+export class ArticleFormComponent {}
