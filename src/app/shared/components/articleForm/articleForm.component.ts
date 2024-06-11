17d7326dd9f3ffde8f20875e2baa610839c25459
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-11 13:54:56 +0200

    article form continued

--- src/app/shared/components/articleForm/articleForm.component.ts
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mc-create-article',
+  selector: 'mc-article-form',
   templateUrl: './articleForm.component.html',
   standalone: true,
 })
