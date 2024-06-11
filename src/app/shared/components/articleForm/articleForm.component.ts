0e835f0ca90f5cd0b04124950339dd2bd038a5e2
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-11 15:10:58 +0200

    article form started

--- src/app/shared/components/articleForm/articleForm.component.ts
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ArticleFormValuesInterface } from './types/articleFormValues.interface';
+import { BackendErrorsInterface } from '../../types/backendErrors.interface';
 
 @Component({
   selector: 'mc-article-form',
   templateUrl: './articleForm.component.html',
   standalone: true,
 })
-export class ArticleFormComponent {}
+export class ArticleFormComponent {
+  @Input() initialValues?: ArticleFormValuesInterface;
+  @Input() isSubmitting: boolean = false;
+  @Input() errors: BackendErrorsInterface | null = null;
+
+  @Output() articlesSubmit = new EventEmitter<ArticleFormValuesInterface>();
+}
