d0d36b40f4f07a30d59ab539d0ca2d5fd1cccd13
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-15 16:26:00 +0200

    create article

--- src/app/shared/components/articleForm/articleForm.component.ts
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -1,14 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ArticleFormValuesInterface } from './types/articleFormValues.interface';
 import { BackendErrorsInterface } from '../../types/backendErrors.interface';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BackendErrorMessages } from '../backendErrorMessages/backendErrorMessages.component';
 
 @Component({
   selector: 'mc-article-form',
   templateUrl: './articleForm.component.html',
   standalone: true,
+  imports: [CommonModule, BackendErrorMessages, ReactiveFormsModule],
 })
-export class ArticleFormComponent {
+export class ArticleFormComponent implements OnInit {
   @Input() initialValues?: ArticleFormValuesInterface;
   @Input() isSubmitting: boolean = false;
   @Input() errors: BackendErrorsInterface | null = null;
@@ -23,4 +26,29 @@ export class ArticleFormComponent {
   });
 
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    if (!this.initialValues) {
+      throw new Error('Inputs are not provided');
+    }
+    this.form.patchValue({
+      title: this.initialValues.title,
+      description: this.initialValues.description,
+      body: this.initialValues.body,
+      tagList: this.initialValues.tagList.join(' '),
+    });
+  }
+
+  onSubmit(): void {
+    const formValue = this.form.getRawValue();
+    const articleFormValues: ArticleFormValuesInterface = {
+      ...formValue,
+      tagList: formValue.tagList.split(' '),
+    };
+    this.articlesSubmit.emit(articleFormValues);
+  }
 }
