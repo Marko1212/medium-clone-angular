a3ad6ddae8c644331c03a54fb2c5d5e9bee7b14f
Marko Askovic <marko.askovic@astree-software.fr>
2024-06-25 09:08:44 +0200

    started user profile

---
+++ src/app/userProfile/userProfile.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { UserProfileComponent } from './components/userProfile/userProfile.component';
+
+export const routes: Route[] = [
+  {
+    path: '',
+    component: UserProfileComponent,
+  },
+];
